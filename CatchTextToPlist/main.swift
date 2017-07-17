5012b340e8dfd011d99eda8deca2f15c779d098c
蕭安億 <x6714h2@yahoo.com.tw>
2017-07-17 16:22:41 +0800

    Initial Commit

---
+++ CatchTextToPlist/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CatchTextToPlist
+//
+//  Created by 蕭安億 on 2017/7/17.
+//  Copyright © 2017年 5hiny. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
